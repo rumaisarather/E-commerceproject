b1615a3164fc1b42168d4aa0dd0695a116d089cc
Rumaisa Rather <144339987+rumaisarather@users.noreply.github.com>
2023-10-29 17:39:15 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,54 @@
+
+<footer class="bg-dark text-white pt-4 pb-4">
+            <div class="container-fluid text-center text-md-left">
+                <div class="row text-center text-md-left">
+                    <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                        <h5 class="text-uppercase mb-4 fw-bold text-warning">Company Name</h5>
+                        <p>Trends-The clothing brand</p>
+                        <p>&copy; 2023 my clothing store. All rights reserved.</p>
+                    </div>
+                    <div class="col-md-2 col-lg2 col-xl-2 mx-auto mt-3">
+                        <h5 class="text-uppercase mb-4 fw-bold text-warning">Products</h5>
+                        <p> 
+                            <a href="#" class="text-white" style="text-decoration: none;">The Popularity</a>
+                        </p>
+                        <p> 
+                            <a href="#" class="text-white" style="text-decoration: none;">The services</a>
+                        </p>
+                        <p> 
+                            <a href="#" class="text-white" style="text-decoration: none;">The Reviews</a>
+                        </p>
+                    </div>
+                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                        <h5 class="text-uppercase mb-4 fw-bold text-warning">Useful links</h5>
+                        <p> 
+                            <a href="#" class="text-white" style="text-decoration: none;">Services</a>
+                        </p>
+                        <p> 
+                            <a href="#" class="text-white" style="text-decoration: none;">Help</a>
+                        </p>    
+                    </div> 
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                        <h5 class="text-uppercase mb-4 fw-bold text-warning">Contact us</h5>
+                        <p> 
+                            <i class="fas fa-home mr-4"></i>India
+                        </p>
+                        <p> 
+                        <i class="fas fa-envelope mr-4"></i>trends@gmail.com
+                        </p>
+                        <P>
+                            <i class="fas fa-phone mr-4"></i>+774-45655768743
+                        </P> 
+                </div>
+            </div>
+
+
+    <div class="icons">
+    <i class="fa fa-facebook"></i>
+    <i class="fa fa-twitter"></i>
+    <i class="fa fa-linkedin"></i>
+     </div>
+        </footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CLOTHING BRAND</title>
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://kit.fontawesome.com/89f77e38cb.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="ecommerce.css"/>
+</head>
+<body>
+
+   <nav class="navbar navbar-expand-lg navbar-dark bg-danger py-3">
+   <div class="container-fluid p-0 m-0 ">
+    <a class="navbar-brand" href="#">Trends!</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse nav-buttons" id="navbarSupportedContent">
+      <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" href="https://localhost/rumi/views/index.php" target="blank">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://localhost/rumi/views/shops.php" target="blank">Shops</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://localhost/rumi/views/blogs.php" target="blank">Blogs</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://localhost/rumi/views/contact.php" target="blank">Contact
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="https://localhost/rumi/views/cart.php"><i class="fas fa-shopping-cart"></i></a>
+          <a href="https://localhost/rumi/views/account.php"><i class="fas fa-user"></i></a>
+        </li>
+    </div>
+  </div>
+</nav>
+
+
+
